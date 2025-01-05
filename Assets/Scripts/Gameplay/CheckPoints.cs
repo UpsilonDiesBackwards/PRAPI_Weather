0602a244a6cb3922d55a4b4c6777fbb18f13669d
Tucccc <j5l3t53@gmail.com>
2025-01-05 18:09:15 +0000

    All mine done I think
    2 Levels in with checkpoints working can select location and screens for finishing the levels, way to quit game and go back to level select and some otherthings i cant remeber right now i think :)

---
+++ Assets/Scripts/Gameplay/CheckPoints.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CheckPoints : MonoBehaviour
+{
+    //Variables
+    public GameObject[] checkPoints;
+    public int curCheckPoint;
+
+    public int chechPointsInLevel;
+
+    public GameObject nextLevelScreen;
+
+    public void Start()
+    {
+        //Assign an id to each checkpoint
+        for(int i = 0; i < checkPoints.Length; i++)
+        {
+            if (checkPoints[i].TryGetComponent(out ObjectID objectID))
+            {
+                objectID.id = i;
+            }
+            else
+            {
+                Debug.LogWarning($"GameObject {checkPoints[i].name} is missing an ID");
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if(curCheckPoint >= chechPointsInLevel)
+        {
+           nextLevelScreen.SetActive(true);
+        }
+    }
+
+
+    //Cleans up array list NOT WORKING CURRENTLY!!!
+    //public void RemoveCompetedCheckPoints()
+    //{
+    //    checkPoints = checkPoints.Where(i => i != null).ToArray();
+    //}
+
+
+}
